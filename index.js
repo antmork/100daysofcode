cc9e6891224ee338106b1a4ad89680488897cecc
Anton <inprogger@gmail.com>
2017-05-09 22:01:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,27 @@ io.on('connection', function(socket){
 		data_y.socket_id = socket.id;
 		io.emit('sockets_22_y', data_y);
 	});
+	
+	socket.on('sockets_23', function(data){
+	data.socket_id = socket.id;
+	io.emit('sockets_23', data);
+	});
+	socket.on('sockets_23_x', function(data_x){
+		data_x.socket_id = socket.id;
+		io.emit('sockets_23_x', data_x);
+	});
+	socket.on('sockets_23_y', function(data_y){
+		data_y.socket_id = socket.id;
+		io.emit('sockets_23_y', data_y);
+	});
+	socket.on('sockets_23_r', function(data_r){
+		data_r.socket_id = socket.id;
+		io.emit('sockets_23_r', data_r);
+	});
+	socket.on('sockets_23_col', function(data_col){
+		data_col.socket_id = socket.id;
+		io.emit('sockets_23_col', data_col);
+	});
 });
 
 http.listen(port, function(){
